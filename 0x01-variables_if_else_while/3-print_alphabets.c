7bb0213ce30c94120f9b01d477696cf1ba3c0101
OcheWest23 <immaxon2017@gmail.com>
2023-07-15 06:59:17 -0700

    Task-3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char n;
+
+	char m;
+
+	n = 'a';
+	m = 'A';
+	while
+		(n <= 'z') {
+			putchar(n);
+			n++;
+		}
+	while
+		(m <= 'Z') {
+			putchar(m);
+			m++;
+		}
+	putchar('\n');
+	return (0);
+}
