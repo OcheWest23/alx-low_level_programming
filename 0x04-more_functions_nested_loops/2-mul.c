ebcd51436b20cea8cf9f7aa8ef2f125860d2b6d0
OcheWest23 <immaxon2017@gmail.com>
2023-07-20 11:34:24 -0700

    Task-2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - func. checs for digit (0)
+ * @x: x - var
+ * @y: y - var
+ * Return: Always 0
+ */
+
+int mul(int x, int y)
+{
+	int mul;
+
+	mul = x * y;
+	return (mul);
+}
