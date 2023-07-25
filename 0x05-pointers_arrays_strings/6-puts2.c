abf852f7fe05d727ed25f4a9f388e8c1eb33dc2a
OcheWest23 <immaxon2017@gmail.com>
2023-07-24 21:07:34 -0700

    puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,20 +10,21 @@ void puts2(char *str)
 	int longi = 0;
 	int t = 0;
 	char *y = str;
-	int o;
+	int d;
 
 	while (*y != '\0')
 	{
+
 		y++;
 		longi++;
 	}
 	t = longi - 1;
-	for (o = 0 ; o <= t ; 0++)
+	for (d = 0 ; d <= t ; d++)
 	{
-		if (o % 2 == 0)
+		if (d % 2 == 0)
 	{
-		_putchar(str[o]);
+		_putchar(str[0]);
 	}
 	}
-	_putchar('\n');
+	_putchar('\n')
 }
