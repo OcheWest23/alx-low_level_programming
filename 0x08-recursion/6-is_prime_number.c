b3fe3882e111842d8690cdfa6c6d8eee0a0e2f28
OcheWest23 <immaxon2017@gmail.com>
2023-08-03 05:53:35 -0700

    6-prime-numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,40 +1,46 @@
 #include "main.h"
 /**
- * prime_a - Calculate if its a prime
- * @a: input
- * @b: divisor
- * Return: (0)
+ * helperfunction - returns 0 or 1.
+ * @num: number being cracked.
+ * @i: possible factor of the number.
+ *
+ * Return: 0 if not prime.  1 if prime.
  */
-int prime_a(int a, int b)
+int helperfunction(int num, int i)
 {
-	if (a <= 1 || (a != &&a % b == 0))
+	if (i < num)
 	{
-		return (0);
+		if (num % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (helperfunction(num, i + 1));
+		}
 	}
-	else if (a == b)
+	else
 	{
 		return (1);
 	}
-	return (prime_a(a, b + 1));
 }
 
 /**
- * is_prime_number - checks if its a prime no
- * @n: the input number
- * Return: 0 or 1
+ * is_prime_number - checks if number is prime or not.
+ * @n: number to be checked.
+ *
+ * Return: 1 if number is prime.
+ * 0 if number is not prime.
  */
+
 int is_prime_number(int n)
 {
-	return (prime_a(n, 2));
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (helperfunction(n, 2));
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
