c942f350af4d90213c7827b37b3d14572f31e65d
OcheWest23 <immaxon2017@gmail.com>
2023-08-03 02:21:06 -0700

    1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,14 +2,16 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse.
- * @s: pointer to string.
+ *
+ * @s: the string to be reversed.
+ *
  * Return: void
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar_rev_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
