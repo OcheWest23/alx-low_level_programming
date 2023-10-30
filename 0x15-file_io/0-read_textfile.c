1a9d618e8224792d2085887503c807bc472138ee
OcheWest23 <immaxon2016@gmail.com>
2023-10-30 06:42:20 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,35 +3,35 @@
 /**
  * read_textfile - Function reads a text file and prints the letters
  * @filename: filename.
- * @letters: Numbers of letters to be printed.
+ * @letters: Number of letters to be printed.
  *
- * Return: The numbers of letters printed. Returns 0, if it fails.
+ * Return: Numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int maxx;
-	ssize_t rot, not;
+	int fd;
+	ssize_t nrd, max;
 	char *buf;
 
 	if (!filename)
 		return (0);
 
-	maxx = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-	if (maxx == -1)
+	if (fd == -1)
 		return (0);
 
 	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
 
-	not = read(maxx, buf, letters);
-	rot = write(STDOUT_FILENO, buf, not);
+	nrd = read(fd, buf, letters);
+	max = write(STDOUT_FILENO, buf, nrd);
 
-	close(maxx);
+	close(fd);
 
 	free(buf);
 
-	return (not);
+	return (max);
 }
 
